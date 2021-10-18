e54f090539cd22b7045d2f328859490b40aae6e3
Alvin Johnson So <so.alvinjohnson@gmail.com>
2021-10-18 14:19:49 +0800

    added new test

--- tests/SomethingTest.php
+++ tests/SomethingTest.php
@@ -24,4 +24,13 @@ class SomethingTest extends TestCase {
 	public function should_equal_true_another() {
 		$this->assertTrue(Something::weird());
 	}
+
+	/**
+	 * @test
+	 * @group weird
+	 * @covers ::weird()
+	 */
+	public function should_equal_true_another_one() {
+		$this->assertTrue(Something::weird());
+	}
 }
