b688976f6a5b29e2e961f0d88639beccfbf1c725
Alvin Johnson So <so.alvinjohnson@gmail.com>
2021-10-18 19:56:45 +0800

    added more test

--- tests/SomethingTest.php
+++ tests/SomethingTest.php
@@ -42,4 +42,13 @@ class SomethingTest extends TestCase {
 	public function should_equal_true_one_more() {
 		$this->assertTrue(Something::weird());
 	}
+
+	/**
+	 * @test
+	 * @group weird
+	 * @covers ::weird()
+	 */
+	public function should_equal_true_extra() {
+		$this->assertTrue(Something::weird());
+	}
 }
