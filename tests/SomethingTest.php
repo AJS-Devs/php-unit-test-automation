4cea6b23475a3b38423942c830fba40cac738a98
Alvin Johnson So <so.alvinjohnson@gmail.com>
2021-10-18 13:29:04 +0800

    test fix #1

--- tests/SomethingTest.php
+++ tests/SomethingTest.php
@@ -15,4 +15,13 @@ class SomethingTest extends TestCase {
 	public function should_equal_true() {
 		$this->assertTrue(Something::weird());
 	}
+
+	/**
+	 * @test
+	 * @group weird
+	 * @covers ::weird()
+	 */
+	public function should_equal_true_another() {
+		$this->assertTrue(Something::weird());
+	}
 }
